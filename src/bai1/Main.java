541bb90c929fda86e03d90c7a7c2d2a500757c40
TuanDao2602 <tuanktktcn@gmail.com>
2022-10-18 23:04:48 +0700

    thuchanh

---
+++ src/bai1/Main.java
@@ -0,0 +1,12 @@
+package bai1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main thread running ....");
+        RunnableDemo runnableDemo =new RunnableDemo("Thread-1-HR-Database");
+        runnableDemo.start();
+        RunnableDemo runnableDemo1 =new RunnableDemo("Thread-2-Send-Email1");
+        runnableDemo1.start();
+        System.out.println("Main thread stopped!!!");
+    }
+}
